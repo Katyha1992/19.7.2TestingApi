4c19d00c902985cd0a75cfe9f52dc86503e92014
Екатерина Вахонина <102897264+Katyha1992@users.noreply.github.com>
2022-06-29 14:06:45 +0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,9 @@
+# Действительные данные для авторизации
+valid_email = 'zyryanov_1991@bk.ru'
+valid_password = '7qpJhDVHnXiFSvD'
+
+# Не действительные данные для авторизации
+invalid_email = 'toptop@yandex.ru'
+invalid_password = 'zxcvb'
+
+
